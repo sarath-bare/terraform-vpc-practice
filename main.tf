08d3d3a330c20a01b893de628c75c059b2c4776c
sarath-bare <sarathkbare@gmail.com>
2025-11-29 10:18:37 +0530

    Update main.tf
    changed name adding my name

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_vpc" "main_vpc" {
+resource "aws_vpc" "main_vpc-skb" {
   cidr_block = "10.0.0.0/16"
   tags = {
     Name = "terraform-demo-vpc"
